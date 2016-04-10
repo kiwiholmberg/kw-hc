e229dc8139dab819234db4db54eeea4d8a7e258b
Kiwi Holmberg <kiwi@kiwifoto.se>
2016-04-10 12:14:04 +0200

    Remove semicolons. URL-encode stuff in request.

--- kwhc/static/js/main.js
+++ kwhc/static/js/main.js
@@ -3,19 +3,22 @@
 var KWHC = {
     init: function() {
         // Bind device buttons
-        $('.device__button').on('click', this.controlDevice);
+        $('.device__button').on('click', this.controlDevice)
     },
     controlDevice: function(e) {
-        var action = $(e.currentTarget).data('action');
-        var name = $(e.currentTarget).parent('.device').data('name');
-        var url = window.baseUrl + 'control/device/' + name + '/' + action;
+        var action = $(e.currentTarget).data('action')
+        var name = $(e.currentTarget).parent('.device').data('name')
+
+        var url = window.baseUrl
+                  + 'control/device/'
+                  + encodeURIComponent(name) + '/'
+                  + encodeURIComponent(action)
         $.post(url, function(data) {
-            console.log(data);
-        });
+            console.log(data)
+        })
     }
+}
 
-};
-
-window.baseUrl = '';
-window._wkhc = KWHC;
-window._wkhc.init();
+window.baseUrl = ''
+window._wkhc = KWHC
+window._wkhc.init()
